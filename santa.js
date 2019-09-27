0be2203423cfa72edd037470c16caa43b646065c
Bsilvs95 <brian9550@gmail.com>
2019-09-27 11:02:56 -0400

    homework

--- santa.js
+++ santa.js
@@ -1,11 +1,15 @@
 function deliverPresents(houses) {
 
+  if (houses.length === 1) {
+    console.log('Delivering presents to ' + houses[0])
+  } else {
+    var mid = Math.ceil(houses.length / 2)
+    var firsthalf = houses.slice(0, mid)
+    var secHalf = houses.slice(mid, houses.length)  //no end goes straight to the end
 
-  while ((i = houses.shift()) !== undefined) {
-    console.log('Delivering presents to ' + i)
+    deliverPresents(firsthalf)
+    deliverPresents(secHalf)
   }
-
-
 }
 
 module.exports = deliverPresents
